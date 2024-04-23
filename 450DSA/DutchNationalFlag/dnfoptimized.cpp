19ab38e27056bf8ef80b75b3e7346f3d8bfb1e6b
Mithilesh Sharma <68504882+Mithilesh-create@users.noreply.github.com>
2024-04-23 11:51:18 +0000

    added changes dnf

---
+++ 450DSA/DutchNationalFlag/dnfoptimized.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[11] = {2, 2, 1, 1, 2, 2, 0, 0, 0, 1, 2};
+    int low = 0;
+    int high = 10;
+    while (low < high)
+    {
+        
+    }
+    for (int i = 0; i < 11; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
